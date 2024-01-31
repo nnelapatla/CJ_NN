7c9ed8605aca5f4e3d07696575fbbb639090d2b6
Navi <naveenanelapatla@gmail.com>
2024-01-31 15:55:06 -0600

    Random

---
+++ src/main/java/randomprograms/Palindrome.java
@@ -0,0 +1,25 @@
+package randomprograms;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int number = 1221;  // Replace with the desired number to check
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+
+    static boolean isPalindrome(int num) {
+        int originalNumber = num;
+        int reversedNumber = 0;
+
+        while (num != 0) {
+            reversedNumber = reversedNumber * 10 + num % 10;
+            num /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+}
+}
